8a59ae90856112092a9efa64cc0b9bc3d8f9d8a2
Gedeon <hello@gedeondoescode.com>
2024-05-31 21:04:16 -0400

    reconfigure install script for new submodule

--- scripts/code.bat
+++ scripts/code.bat
@@ -25,9 +25,9 @@ set ELECTRON_ENABLE_STACK_DUMPING=1
 
 :: Get Pear AI
 setlocal
-cd extensions/pearai-extension
-call yarn install
-call yarn build-all
+cd extensions/pearai-submodule
+powershell.exe -executionpolicy bypass -file .\scripts\install-dependencies.ps1
+powershell.exe -executionpolicy bypass -file .\scripts\build-extension.ps1
 endlocal
 
 :: Launch Code
